63c7b2638b9a27c2322f7137c7f18c9917d8b817
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-09-22 21:33:22 -0400

    localdb for testing

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log(process.env.PORT);
+const express = require("express");
+const app = express();
+const connectDB = require("./db/db_connection");
+
+// setup middleware
+app.use(express.json());
+
+// Connect Database
+connectDB();
