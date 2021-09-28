b21f2b956bd011e54f7ca4be7fc2e42c59c636ea
shashank <shashank.patel@aheeva.com>
2021-09-28 13:26:36 -0400

    test

--- server/db/db_connection.js
+++ server/db/db_connection.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // connect to database
 const connectDB = async () => {
   try {
     await mongoose
-        .connect(process.env.MONGODB_URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        })
-        .then(() => console.log('MongoDB connected...'));
+      .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("MongoDB connected..."));
   } catch (err) {
     console.error(err.message);
     process.exit(1);
--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -12,4 +12,4 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-module.exports = User = mongoose.model('user', userSchema);
+module.exports = User = mongoose.model("user", userSchema);
