e68c0321599dbdef29a101212d22189b7d8f9521
shashank <shashank.patel@aheeva.com>
2021-10-04 19:16:27 -0400

    turned off linebreak rule due to windows CRLF and unix LF issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable require-jsdoc */
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
