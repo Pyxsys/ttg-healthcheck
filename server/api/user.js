58b6c605988da0031fd5f982ec73b373850fd956
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-10-08 20:35:00 -0400

    login, logout
    login user, log out user( remove cookie)

--- server/api/user.js
+++ server/api/user.js
@@ -19,10 +19,12 @@ router.post('/register', async (req, res) => {
     const salt = await bcrypt.genSalt(10)
     // Hash password
     newUser.password = await bcrypt.hash(password, salt)
+    // check if email already in use
     let user = await User.findOne({ email })
     if (user) {
       return res.status(400).json({ errors: [{ msg: 'User already exists' }] })
     }
+    // save new user, create JWT, store in cookie and send to front-end
     await newUser.save()
     const token = jwt.sign(
       { name: name, role: role },
@@ -42,6 +44,49 @@ router.post('/register', async (req, res) => {
     console.error(err.message)
     res.status(500).send('Server Error')
   }
+})
+
+// login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    // Verify email
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.status(400).json({ errors: [{ msg: 'Email does not exist' }] })
+    }
+    // Verify pw matches ( pw against encrypted pw)
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      return res.status(400).json({ errors: [{ msg: 'Invalid Password' }] })
+    }
+    // Create JWT, store in cookie and send to front-end
+    const token = jwt.sign(
+      { name: user.name, role: user.role },
+      process.env.ACCESS_TOKEN,
+      {
+        expiresIn: '1h',
+      }
+    )
+    return res
+      .cookie('access_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+      })
+      .status(200)
+      .json({ message: 'Logged in' })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
+// log out user
+router.get('/logout', (req, res) => {
+  return res
+    .clearCookie('access_token')
+    .status(200)
+    .json({ message: 'Successfully logged out' })
 })
 
 module.exports = router
