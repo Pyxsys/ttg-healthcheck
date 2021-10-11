6ea0496a42eba237870a00d9b62ca832cf65f2a4
Pyxsys <barras.pierre.a@gmail.com>
2021-10-11 10:38:09 -0400

    adding scrapper implementation #7

--- daemon/src/system_report.py
+++ daemon/src/system_report.py
@@ -1,8 +1,32 @@
+import requests
 import psutil
 import json
 
-class SysReport:
+class Runner:
 
+    api_endpoint='api/daemon_endpoint'
+
+    # Initializes instance attributes.
+    def __init__(self, path):
+        with open(path, "r") as config_file:
+            self.configs = json.load(config_file)
+
+
+    def getConfig(self):
+        return self.configs
+
+    def getReport(self):
+        return self.report.report_message
+
+    def sendReport(self):
+        url = self.getConfig()['destination'] + self.api_endpoint
+        return requests.post(url, json=self.getReport())
+
+    def genReport(self):
+        self.report=SysReport()
+        self.report.addSystemProcessInfo()
+
+class SysReport:
     # Initializes instance attributes.
     def __init__(self):
         self.report_message = {}
