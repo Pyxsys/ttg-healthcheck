12358c9dd5079d6b52f385a5aed5f692fdc0fd05
Derek <ruizc.derek@gmail.com>
2021-10-28 01:40:48 -0400

    Server Test - Adds Testing For MongoDB Change Stream
    Test inserting object into database and receiving insert information from the web socket

---
+++ server/__tests__/change_streams.test.js
@@ -0,0 +1,96 @@
+const request = require('supertest')
+const connectDB = require('../db/db_connection')
+const User = require('../models/user.js')
+const app = require('../app')
+const mongoose = require('mongoose')
+const WebSocket = require('ws')
+const {
+  createChangeStream,
+  attachNotifyClients,
+  monitorPredefinedCollections,
+  closeMonitoredCollection,
+} = require('../db/change_streams')
+const { attachWSConnection } = require('../websocket')
+const clientCollections = require('../db/ws_clients')
+
+const PORT = 80
+const collectionName = 'users'
+
+const testValues = {
+  wsServer: null,
+  changeStream: null,
+  wsClient: null,
+}
+const userOne = {
+  name: 'test',
+  password: 'password',
+  email: 'test@gmail.com',
+  role: 'user',
+}
+
+const closeWebSocket = () => {
+  if (testValues.wsClient) {
+    testValues.wsClient.close()
+    testValues.wsClient = null
+  }
+}
+
+beforeAll(async () => {
+  // create websocket server
+  testValues.wsServer = app.listen(PORT, () =>
+    console.log(`Web socket listening on port ${PORT}`)
+  )
+  attachWSConnection(testValues.wsServer)
+
+  // connect to local_db
+  await connectDB().then(async () => {
+    testValues.changeStream = await createChangeStream(collectionName)
+    await attachNotifyClients(testValues.changeStream)
+    await monitorPredefinedCollections()
+  })
+
+  await User.deleteMany()
+})
+
+describe('Insert a new user to the database', () => {
+  it('should receive the inserted data from the websocket ', (done) => {
+    closeWebSocket()
+    testValues.wsClient = new WebSocket(
+      `ws://localhost:${PORT}/?collection=${collectionName}`
+    )
+
+    testValues.wsClient.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      expect(data.fullDocument).toBeDefined()
+      expect(data.operationType).toBe('insert')
+      closeWebSocket()
+      done()
+    }
+
+    request(app)
+      .post('/api/user/register')
+      .send(userOne)
+      .then((response) => {
+        expect(response.statusCode).toBe(200)
+      })
+  })
+})
+
+afterAll(async () => {
+  // Close client web socket if hanging
+  closeWebSocket()
+  // Close change stream
+  if (testValues.changeStream) {
+    testValues.changeStream.close()
+  }
+  // Close predefined collections
+  await closeMonitoredCollection(Object.keys(clientCollections)[0])
+  await closeMonitoredCollection()
+
+  // Close web socket server
+  if (testValues.wsServer) {
+    testValues.wsServer.close()
+  }
+  // Closing the DB connection allows Jest to exit successfully.
+  mongoose.connection.close()
+})
--- server/db/change_streams.js
+++ server/db/change_streams.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const clientCollections = require('./ws_clients')
 
+var changeStreamsMonitored = []
 const pipeline = {
   $match: {
     $or: [{ operationType: 'insert' }, { operationType: 'update' }],
@@ -8,34 +9,79 @@ const pipeline = {
   },
 }
 
-const monitorChangeStream = async (collectionName) => {
+/**
+ * Creates a change stream for a given collection.
+ * If the collection does not exist it will be created
+ * implicitly. Returns the created change stream.
+ * @param {String} collectionName
+ * @returns a change stream
+ */
+const createChangeStream = async (collectionName) => {
   const client = mongoose.connection.getClient()
-  const db = client.db('database')
+  const db = client.db(process.env.MONGO_DB)
   const collection = db.collection(collectionName)
-  const changeStream = collection.watch([pipeline])
+  return collection.watch([pipeline])
+}
 
+/**
+ * Attach a change stream to notify the clients
+ * when the change stream recieves a change.
+ * @param {ChangeStream<Document>} changeStream
+ */
+const attachNotifyClients = async (changeStream) => {
   // When there is a change to the collection
   changeStream.on('change', (next) => {
-    clientCollections[collectionName].forEach((client) => {
+    clientCollections[changeStream.parent.collectionName].forEach((client) => {
       if (client.readyState === client.OPEN) {
         client.send(JSON.stringify(next))
       }
     })
   })
+}
 
-  // When the process exits, close the change stream
-  process.on('exit', () => {
-    changeStream.close()
-    console.log(
-      `Closed MongoDB Change Stream for collection: ${collectionName}`
-    )
+/**
+ * Monitor a collection to notify clients when
+ * the collection receives an update.
+ * @param {String} collectionName
+ */
+const monitorCollection = async (collectionName) => {
+  const changeStream = await createChangeStream(collectionName)
+  changeStreamsMonitored.push(changeStream)
+  attachNotifyClients(changeStream)
+}
+
+/**
+ * Monitor all the predefined collections to notify
+ * clients when the collection receives an update.
+ */
+const monitorPredefinedCollections = async () => {
+  Object.keys(clientCollections).forEach(async (dbCollection) => {
+    await monitorCollection(dbCollection)
   })
 }
 
-const monitorAllChangeStreams = async () => {
-  Object.keys(clientCollections).forEach((dbCollection) => {
-    monitorChangeStream(dbCollection)
+/**
+ * Close the selected monitored collections. If collectionName
+ * is null then close all the open monitor collections.
+ * @param {String} collectionName
+ */
+const closeMonitoredCollection = async (collectionName) => {
+  changeStreamsMonitored = changeStreamsMonitored.filter((changeStream) => {
+    if (
+      !collectionName ||
+      changeStream.parent.collectionName === collectionName
+    ) {
+      changeStream.close()
+      return false
+    }
+    return true
   })
 }
 
-module.exports = { monitorChangeStream, monitorAllChangeStreams }
+module.exports = {
+  createChangeStream,
+  attachNotifyClients,
+  monitorCollection,
+  monitorPredefinedCollections,
+  closeMonitoredCollection,
+}
