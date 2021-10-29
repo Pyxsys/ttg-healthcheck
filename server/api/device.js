85c6664fb6cdd227cd66b575cd76bdc8304c352b
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-10-29 17:09:13 -0400

    api/device.js - multiple device with options update
    can do by limit, and do by limit and an attribute + value of attribute

--- server/api/device.js
+++ server/api/device.js
@@ -5,18 +5,41 @@ const Devices = require('../models/device.js')
 const auth = require('../middleware/auth.js')
 
 // get a specific device, based on Id, or name
-router.get('/specific', auth, async (req, res) => {
+router.get('/specific', async (req, res) => {
   try {
     let param = req.query.entry
-    Devices.findOne(
-      { $or: [{ deviceId: param }, { name: param }] },
-      function (err, device) {
-        if (err) {
-          res.send(err)
-        }
-        res.status(200).json(device)
-      }
-    )
+    await Devices.findOne({
+      $or: [{ deviceId: param }, { name: param }],
+    }).exec(function (err, device) {
+      res.status(200).json(device)
+    })
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
+// get multiple devices with options
+router.get('/multiple', async (req, res) => {
+  try {
+    let limit = req.query.limit
+    let attribute = req.query.attribute
+    let attribute_value = req.query.attribute_value
+    if (!!limit && !!attribute && !!attribute_value) {
+      await Devices.find({ [attribute]: attribute_value })
+        .limit(parseInt(limit))
+        .exec(function (err, device) {
+          res.status(200).json(device)
+        })
+    } else if (!!limit) {
+      await Devices.find()
+        .limit(parseInt(limit))
+        .exec(function (err, device) {
+          res.status(200).json(device)
+        })
+    } else {
+      return res.status(200).json('nope')
+    }
   } catch (err) {
     console.error(err.message)
     res.status(500).send('Server error')
