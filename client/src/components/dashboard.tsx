c9990a22cbbac0ca14f1d69dfbba67f739f4ada7
j-eltayar <eltayarjames@gmail.com>
2021-10-29 12:43:27 -0400

    Created Standard Navbar Base #51
    Added a navbar component that acts as a basic navbar once logged in

--- client/src/components/dashboard.tsx
+++ client/src/components/dashboard.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {useAuth} from '../context/authContext';
+import Navbar from './nav';
+import {Col, Row} from 'react-bootstrap';
 
 const Dashboard = () => {
   const {user} = useAuth();
   return (
     <div>
-      dashboard, user: {user.name}, role: {user.role}
+      <Row className="h-100">
+        <Navbar />
+        <Col>
+          <div className="">
+            dashboard, user: {user.name}, role: {user.role}
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
---
+++ client/src/components/nav.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import {Col, Container, Nav, Row} from 'react-bootstrap';
+import {IconContext} from 'react-icons';
+import {MdOutlineSpaceDashboard, MdAnalytics, MdLogout} from 'react-icons/md';
+import {DiRasberryPi} from 'react-icons/di';
+import {CgProfile} from 'react-icons/cg';
+import axios from 'axios';
+import {useAuth} from '../context/authContext';
+
+const Navbar = () => {
+  const {setUser, setIsAuthenticated} = useAuth();
+
+  const logout = async (e: React.ChangeEvent<any>) => {
+    e.preventDefault();
+    await axios
+        .get('api/user/logout')
+        .then((response) => {
+          if (response.data) {
+            console.log(response.data);
+            setUser({name: '', role: ''});
+            setIsAuthenticated(false);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  };
+
+  return (
+    <>
+      <Col className="h-100 col-auto">
+        <Nav className="h-100 bg-primary" activeKey="/dashboard">
+          <Container className=" flex align-items-start flex-column h-100 mt-5 mb-5">
+            <Row className="p-2">
+              <Col>
+                <Nav.Item>
+                  <Nav.Link className="text-secondary mt-5" href="/dashboard">
+                    <IconContext.Provider value={{size: '2em'}}>
+                      <div>
+                        <MdOutlineSpaceDashboard />
+                      </div>
+                    </IconContext.Provider>
+                  </Nav.Link>
+                </Nav.Item>
+              </Col>
+            </Row>
+            <Row className="p-2">
+              <Col>
+                <Nav.Item>
+                  <Nav.Link className="text-secondary mt-5" href="/dashboard">
+                    <IconContext.Provider value={{size: '2em'}}>
+                      <div>
+                        <DiRasberryPi />
+                      </div>
+                    </IconContext.Provider>
+                  </Nav.Link>
+                </Nav.Item>
+              </Col>
+            </Row>
+            <Row className="p-2">
+              <Col>
+                <Nav.Item>
+                  <Nav.Link className="text-secondary mt-5" href="/dashboard">
+                    <IconContext.Provider value={{size: '2em'}}>
+                      <div>
+                        <MdAnalytics />
+                      </div>
+                    </IconContext.Provider>
+                  </Nav.Link>
+                </Nav.Item>
+              </Col>
+            </Row>
+            <Row className="p-2">
+              <Col>
+                <Nav.Item>
+                  <Nav.Link className="text-secondary mt-5" href="/dashboard">
+                    <IconContext.Provider value={{size: '2em'}}>
+                      <div>
+                        <CgProfile />
+                      </div>
+                    </IconContext.Provider>
+                  </Nav.Link>
+                </Nav.Item>
+              </Col>
+            </Row>
+            <Row className="mb-auto p-2">
+              <Col>
+                <Nav.Item>
+                  <Nav.Link
+                    className="text-secondary mt-5"
+                    href="/logout"
+                    onClick={(e) => logout(e)}
+                  >
+                    <IconContext.Provider value={{size: '2em'}}>
+                      <div>
+                        <MdLogout />
+                      </div>
+                    </IconContext.Provider>
+                  </Nav.Link>
+                </Nav.Item>
+              </Col>
+            </Row>
+          </Container>
+        </Nav>
+      </Col>
+    </>
+  );
+};
+export default Navbar;
