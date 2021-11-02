533b5ff6a54e6a48c2d437ae1ca7c9bf61dfcba9
shashank <shashank.patel@aheeva.com>
2021-11-02 18:09:55 -0400

    server api - fix duplicate code issue

--- server/api/device.js
+++ server/api/device.js
@@ -3,40 +3,7 @@ const express = require('express')
 const router = express.Router()
 const Devices = require('../models/device.js')
 const auth = require('../middleware/auth.js')
-
-// String attributes list to check with req.query object
-const stringAttributes = [
-  'orderBy',
-  'deviceId',
-  'name',
-  'description',
-  'connectionType',
-  'status',
-  'provider',
-  'memory.formFactor',
-  'hardware.hardwareName',
-  'disk.type',
-]
-
-// Number attributes list to check with req.query object
-const numberAttributes = [
-  'limit',
-  'orderValue',
-  'cpu.baseSpeed',
-  'cpu.sockets',
-  'cpu.cores',
-  'cpu.processors',
-  'cpu.cacheSizeL1',
-  'cpu.cacheSizeL2',
-  'cpu.cacheSizeL3',
-  'wifi.adapterName',
-  'wifi.SSID',
-  'wifi.connectionType',
-  'wifi.ipv4Address',
-  'wifi.ipv6Address',
-  'memory.maxSize',
-  'disk.capacity',
-]
+const filterData = require('./shared/filter')
 
 // get a specific device, based on param option of either deviceId or name
 router.get('/specific-device', async (req, res) => {
@@ -56,35 +23,7 @@ router.get('/specific-device', async (req, res) => {
 // get multiple devices with param options (limit, multiple attributes, orderBy, orderValue)
 router.get('/options', async (req, res) => {
   try {
-    let options = {}
-    let query = {}
-    let queryObj = Object(req.query)
-    for (let k in queryObj) {
-      queryObj[k] = queryObj[k].split(',')
-      if (stringAttributes.includes(k)) {
-        query[String(k)] = queryObj[k]
-      }
-      if (numberAttributes.includes(k)) {
-        query[String(k)] = queryObj[k].map(Number)
-      }
-    }
-    if (query.limit) {
-      options.limit = query.limit
-      delete query.limit
-    }
-    if (query.orderBy) {
-      var orderBy = query.orderBy
-      delete query.orderBy
-    }
-    if (query.orderValue) {
-      var orderValue = query.orderValue
-      delete query.orderValue
-    }
-    if (orderValue && orderBy) {
-      options.sort = {
-        [orderBy]: orderValue,
-      }
-    }
+    let query = filterData(req.query)
     await Devices.find({ $and: [query] }, {}, options).exec((err, device) => {
       return res.status(200).json(device)
     })
--- server/api/memoryLogs.js
+++ server/api/memoryLogs.js
@@ -1,21 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Memory = require('../models/memory.js')
-
-// String attributes list to check with req.query object
-const stringAttributes = ['orderBy', 'deviceId', 'timestamp']
-
-// Number attributes list to check with req.query object
-const numberAttributes = [
-  'limit',
-  'orderValue',
-  'usagePercentage',
-  'inUse',
-  'available',
-  'cached',
-  'pagedPool',
-  'nonPagedPool',
-]
+const filterData = require('./shared/filter')
 
 // get X number of entries for single device (limit, deviceId)
 router.get('/specific-device', async (req, res) => {
@@ -74,35 +60,7 @@ router.get('/timestamp', async (req, res) => {
 // get multiple entries given an attribute with the ability to add a limit and order by filter
 router.get('/specific-attribute', async (req, res) => {
   try {
-    let options = {}
-    let query = {}
-    let queryObj = Object(req.query)
-    for (let k in queryObj) {
-      queryObj[k] = queryObj[k].split(',')
-      if (stringAttributes.includes(k)) {
-        query[String(k)] = queryObj[k]
-      }
-      if (numberAttributes.includes(k)) {
-        query[String(k)] = queryObj[k].map(Number)
-      }
-    }
-    if (query.limit) {
-      options.limit = query.limit
-      delete query.limit
-    }
-    if (query.orderBy) {
-      var orderBy = query.orderBy
-      delete query.orderBy
-    }
-    if (query.orderValue) {
-      var orderValue = query.orderValue
-      delete query.orderValue
-    }
-    if (orderValue && orderBy) {
-      options.sort = {
-        [orderBy]: orderValue,
-      }
-    }
+    let query = filterData(req.query)
     await Memory.MemoryLogs.find({ $and: [query] }, {}, options).exec(
       (err, memoryLogs) => {
         return res.status(200).json(memoryLogs)
