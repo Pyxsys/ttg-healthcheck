f1138b7ec4409a618072beeeca16873d53c718f6
Pyxsys <barras.pierre.a@gmail.com>
2021-11-01 20:31:41 -0400

    [D] Fixing #134

--- daemon/test/test_report_requests.py
+++ daemon/test/test_report_requests.py
@@ -15,15 +15,15 @@ class TestRunner(unittest.TestCase):
         cls.test_runner=Runner(sys.path[0] + '/test/config.json')
         print("\n[config values]:")
         print(json.dumps(cls.test_runner.getConfig(), indent=4, sort_keys=True))
-        
+
     @classmethod
     def tearDownClass(cls):
         del cls.test_runner
-        
+
     # Unit-Tests-----
     def testRunnerClassInitialization(self):
         self.assertIsInstance(self.test_runner, Runner, msg=None)
-   
+
     def testRunnerGeneratingReport(self):
         expected_sections={'deviceId', 'processes', 'timestamp'}
         missing_sections=[]
@@ -34,15 +34,15 @@ class TestRunner(unittest.TestCase):
             if section not in section_list:
                 missing_sections.append(section)
         self.assertEqual(len(missing_sections), 0, msg=missing_sections)
-                
+
     @unittest.skipIf("localhost" in json.load(open(sys.path[0] + '/test/config.json'))['destination'],
      'Cannot run test automatically with "localhost" destination.')
     def testConnectionToServer(self):
-        expected_result = 200
+        expected_result = 500
         url = self.test_runner.getConfig()['destination'] + Runner.api_endpoint
 
         response=requests.post(url)
-        
+
         actual_result = response.status_code
         self.assertEqual(expected_result, actual_result)
 
@@ -53,7 +53,7 @@ class TestRunner(unittest.TestCase):
 
         self.test_runner.genReport()
         response=self.test_runner.sendReport()
-        
+
         actual_result = response.status_code
         self.assertEqual(expected_result, actual_result)
 
@@ -63,17 +63,17 @@ class TestRunner(unittest.TestCase):
         config_file.close()
 
         actual_result = self.test_runner.getConfig()['report_delay']
-        
+
         self.assertEqual(expected_result, actual_result)
 
-    def testRunnerDelay(self): 
+    def testRunnerDelay(self):
         tolerance = 1.10 #A decimal multiplier that defines the upper bound
         config_file = open(sys.path[0] + '/test/config.json')
         permitted_time = json.load(config_file)['report_delay'] * tolerance
         config_file.close()
 
         actual_time = timeit.timeit(lambda: self.test_runner.sleep(), number=1)
-        
+
         self.assertGreater(permitted_time, actual_time)
 
 if __name__ == '__main__':
