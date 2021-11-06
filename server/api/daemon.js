61b968e1fd713fe0d5189fe6029bdf521696962c
arich94 <adamrichard.94@gmail.com>
2021-11-06 11:47:41 -0400

    adds network to daemon endpoitns #10

--- server/api/daemon.js
+++ server/api/daemon.js
@@ -3,13 +3,16 @@ const express = require('express')
 const router = express.Router()
 const cpu = require('../models/cpu.js')
 const process = require('../models/process.js')
+const wifi = require('../models/wifi.js')
 
 // receive report from daemon
 router.post('/', async (req, res) => {
   try {
     const payload = req.body
     let newCpuLog = processCpuLogInfo(payload)
+    let newWifiLog = processWifiLogInfo(payload)
 
+    await newWifiLog.save()
     await newCpuLog.save()
     res.status(200).send()
   } catch (err) {
@@ -64,7 +67,26 @@ function processCpuLogInfo(payload) {
   })
 }
 
+function processWifiLogInfo(payload) {
+  //load values
+  const { deviceId, timestamp, processes } = payload
+
+  //compute values
+  const sendSpeed = 0
+  const receiveSpeed = 0
+  const signalStrength = 0
+
+  return new wifi.WifiLogs({
+    deviceId,
+    sendSpeed,
+    receiveSpeed,
+    signalStrength,
+    timestamp,
+  })
+}
+
 module.exports = {
   router,
   processCpuLogInfo,
+  processWifiLogInfo,
 }
