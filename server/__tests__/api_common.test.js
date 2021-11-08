02226fc21805e5047dfa3c7d5e5fa499180c97d8
Derek <ruizc.derek@gmail.com>
2021-11-07 20:07:25 -0500

    Server Test - Add Test For API Common Functions

---
+++ server/__tests__/api_common.test.js
@@ -0,0 +1,24 @@
+const { filterData } = require('../api/shared/filter')
+
+describe('Filter out attributes from Query', () => {
+  it('should filter out the keyword limit', () => {
+    const req = {
+      deviceId: '1',
+      limit: '1',
+    }
+    const [query, options] = filterData(req)
+    expect(query).toEqual({ deviceId: '1' })
+    expect(options).toEqual({ limit: [1] })
+  })
+
+  it('should filter out the keyword orderBy and orderValue', () => {
+    const req = {
+      deviceId: '1',
+      orderBy: 'timestamp',
+      orderValue: '-1',
+    }
+    const [query, options] = filterData(req)
+    expect(query).toEqual({ deviceId: '1' })
+    expect(options).toEqual({ sort: { timestamp: [-1] } })
+  })
+})
