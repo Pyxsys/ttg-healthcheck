80be845a6413516fdaee808c5f08d4d3711a0c2b
Sacha Elkaim <sachaelkaim87@gmail.com>
2021-11-14 21:01:54 -0500

    front end - set up selenium locally

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
-import App from '../App';
+import {Builder, WebDriver, By, Key, until} from 'selenium-webdriver';
+const chrome = require('selenium-webdriver/chrome');
+const path = require('chromedriver').path;
 
-test('renders learn react link', () => {});
+chrome.setDefaultService(new chrome.ServiceBuilder(path).build());
+
+describe('LoginTest', () => {
+  let driver: WebDriver;
+  beforeAll(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+  afterEach(async () => {
+    await driver.close();
+  });
+  afterAll(async () => {
+    await driver.quit();
+  });
+  it('go to google website, search webdriver', async () => {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  });
+});
