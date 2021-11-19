d4c2e9c9e4db5b2f279210ac0a4d7512d3c299ce
Derek <ruizc.derek@gmail.com>
2021-11-19 17:54:10 -0500

    Server Tests - Adds 100 Percent Server Unit Test Coverage

--- server/api/device.js
+++ server/api/device.js
@@ -6,17 +6,17 @@ const auth = require('../middleware/auth.js')
 const { filterData } = require('./shared/filter')
 
 // get all device id
-router.get('/ids', async (req, res) => {
-  const devices = await Devices.find({}, { deviceId: 1 })
-  res.status(200).json(devices)
+router.get('/ids', auth, async (req, res) => {
+  const [query, options] = filterData(req.query)
+  const devices = await Devices.find(query, { deviceId: 1 }, options)
+  return res.status(200).json({ Results: devices })
 })
 
 // get multiple devices with param options (limit, multiple attributes, orderBy, orderValue)
 router.get('/', auth, async (req, res) => {
-  let [query, options] = filterData(req.query)
-  await Devices.find({ $and: [query] }, {}, options).exec((err, device) => {
-    return res.status(200).json(device)
-  })
+  const [query, options] = filterData(req.query)
+  const results = await Devices.find({ $and: [query] }, {}, options)
+  return res.status(200).json({ Results: results })
 })
 
 module.exports = router
