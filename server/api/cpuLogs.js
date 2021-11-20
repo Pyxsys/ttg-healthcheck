3a315776130c9d307a56343aaeee390a260bcf63
Derek <ruizc.derek@gmail.com>
2021-11-19 20:17:59 -0500

    Server - Refactor Removes Injection Attacks Vulnerability

--- server/api/cpuLogs.js
+++ server/api/cpuLogs.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/auth.js')
 
 // get CPU Logs with any attribute of the CpuLogs Model
 router.get('/', auth, async (req, res) => {
-  const [query, options] = filterData(req.query)
+  const [query, options] = filterData(Object(req.query))
   const results = await Cpu.CpuLogs.find({ $and: [query] }, {}, options)
   return res.status(200).json({ Results: results })
 })
@@ -14,7 +14,7 @@ router.get('/', auth, async (req, res) => {
 // get CPU Logs with any attribute of the CpuLogs Model and within a timestamp (startTimeStamp, endTimeStamp)
 router.get('/timestamp', auth, async (req, res) => {
   try {
-    const [query, options] = filterTimestampQuery(req.query)
+    const [query, options] = filterTimestampQuery(Object(req.query))
     const results = await Cpu.CpuLogs.find(query, {}, options)
     return res.status(200).json({ Results: results })
   } catch (err) {
