11847a3562c8716789d3966416a101a78396bb5c
Pyxsys <barras.pierre.a@gmail.com>
2021-11-20 11:06:54 -0500

    [S] Added device upsert #16

--- server/api/daemon.js
+++ server/api/daemon.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const cpu = require('../models/cpu.js')
+const Device = require('../models/device.js')
 
 // receive report from daemon
 router.post('/', async (req, res) => {
@@ -12,19 +13,67 @@ router.post('/', async (req, res) => {
     await newCpuLog.save()
     res.status(200).send()
   } catch (err) {
-    res.status(500).send('Server Error ' + err.message)
+    res.status(500).send('Server Error: ' + err.message)
   }
 })
 
-function verifyDeviceIdFormat(deviceId){
+// receive device report from daemon
+router.post('/device', async (req, res) => {
+  try {
+    const payload = req.body
+    verifyDeviceIdFormat(payload.deviceId)
+
+    let newDevice = processDeviceInfo(payload)
+
+    const filter = { deviceId: payload.deviceId }
+    const flags = { upsert: true }
+    response = await Device.findOneAndUpdate(filter, newDevice, flags)
+
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send('Server Error: ' + err.message)
+  }
+})
+
+function verifyDeviceIdFormat(deviceId) {
   const pattern = '^[a-zA-Z0-9]{8}(?:-[a-zA-Z0-9]{4}){3}-[a-zA-Z0-9]{12}$'
   const regex = new RegExp(pattern, 'i')
 
-  if (!regex.test(deviceId)){
+  if (!regex.test(deviceId)) {
     throw new Error('deviceId [' + deviceId + '] is invalid')
-  }
-  else return true
+  } else return true
 }
+
+function processDeviceInfo(payload) {
+  const { deviceId, memory_ } = payload
+
+  const name = null
+  const description = null
+  const connectionType = null
+  const status = null
+  const provider = null
+
+  const hardware = null
+  const cpu = null
+  const memory = memory_
+  const disk = null
+  const wifi = null
+
+  return {
+    deviceId,
+    name,
+    description,
+    connectionType,
+    status,
+    provider,
+    hardware,
+    cpu,
+    memory,
+    disk,
+    wifi,
+  }
+}
+
 function sumProcessCpuUsage(processes) {
   let sum = 0
   processes.forEach(function (proc) {
@@ -74,5 +123,6 @@ function processCpuLogInfo(payload) {
 module.exports = {
   router,
   verifyDeviceIdFormat,
+  processDeviceInfo,
   processCpuLogInfo,
 }
