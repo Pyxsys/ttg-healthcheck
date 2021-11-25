75da06ca10992cbaa1a5116bec66ee95ef7c593b
Derek <ruizc.derek@gmail.com>
2021-11-25 00:45:38 -0500

    Client - Create External Page Component
    Create common External Page Component that show the title and copyright

---
+++ client/src/components/common/externalPage.tsx
@@ -0,0 +1,28 @@
+// 3rd Party
+import React from 'react';
+
+// Custom
+import '../../App.scss';
+
+const External = ({children}: {children: JSX.Element}) => {
+  return (
+    <div className="h-100 d-flex flex-column">
+      <div className="d-flex flex-column align-items-center home-header">
+        <div className="flex-grow-1 d-flex flex-column align-items-center justify-content-center px-3">
+          <i className="home-logo"></i>
+        </div>
+        <i className="bottom-triangle"></i>
+      </div>
+
+      <div className="flex-grow-1 d-flex flex-column align-items-center justify-content-center home-content cerebellum-scrollbar">
+        {children}
+      </div>
+
+      <div className="d-flex justify-content-center home-footer">
+        <div className="pt-1 pb-3 home-copyright">&#169; SOEN490 TTG-HEALTCHECK</div>
+      </div>
+    </div>
+  );
+};
+
+export default External;
