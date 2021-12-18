33e346ba9cf2543d133d5082e41f90c9b0644e0d
Derek <ruizc.derek@gmail.com>
2021-12-18 16:00:42 -0500

    Server - Refactor Code Smell
    Add the "let", "const" or "var" keyword to this declaration of "processArray" to make it explicit.

--- server/api/daemon.js
+++ server/api/daemon.js
@@ -146,7 +146,7 @@ const processProcessLogInfo = (payload) => {
   const { processes } = payload
 
   //compute values
-  processArray = new Array()
+  const processArray = new Array()
 
   processes.forEach((element) => {
     processArray.push(processSingleProcess(element))
