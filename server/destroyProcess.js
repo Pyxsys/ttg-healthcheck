62bc88319116376cb60a20efd4364c5334b9bff9
Derek <ruizc.derek@gmail.com>
2021-12-24 18:37:15 -0500

    Server - Add JSDoc for Callback Functions

--- server/destroyProcess.js
+++ server/destroyProcess.js
@@ -1,15 +1,27 @@
+/**
+ * Callback for exiting the process.
+ * @callback ExitCallback
+ * @param {*} err thrown error
+ * @param {number} code error code
+ */
+
+/**
+ * Collection of callback functions for exiting.
+ * @type {ExitCallback[]}
+ */
 const subscriptions = []
 
 /**
- * Performs the specified action when the process is indicated to exit.
- * @param callbackfn A function that accepts two arguments.
- * subscribeOnExit calls the callbackfn function when the
- * process should exit.
+ * Calls the provided function when the process is indicated to exit.
+ * @param {ExitCallback} callbackfn A function that accepts two arguments: **err**, **code**
  */
 const subscribeOnExit = (callbackfn) => {
   subscriptions.push(callbackfn)
 }
 
+/**
+ * Removes all the callback functions that are called when the process exits.
+ */
 const unsubscribeAll = () => {
   subscriptions.splice(0)
 }
