a6abc441e50e8bbe2f6c1ab5a14a49bdf3b126a8
Sacha Elkaim <37674225+sachaelkaim@users.noreply.github.com>
2022-01-11 13:54:56 -0500

    Client - css changes to accordions
    added dark accordion

--- client/src/components/device-detail-widgets/cpuUsage.tsx
+++ client/src/components/device-detail-widgets/cpuUsage.tsx
@@ -10,30 +10,30 @@ const CpuUsageWidget = (props: any) => {
   const deviceDynamic: DeviceLog = props.deviceDynamic;
 
   return (
-    <Col className="device-details-accordion">
+    <Col className="device-details-accordion dark-accordion pt-3 pb-3">
       <Accordion defaultActiveKey="0" flush>
         <Accordion.Item eventKey="0">
           <Accordion.Header>
-            <div className="d-flex w-100">CPU Usage Information</div>
+            <div className="d-flex w-100 justify-content-around">CPU Usage Information</div>
           </Accordion.Header>
           <Accordion.Body>
-            <Table className="device-details-table">
+            <Table className="device-details-table device-details-table-dark">
               <tbody>
                 <tr className="border-bottom">
                   <td className="w-50">Usage</td>
-                  <td>{deviceDynamic?.cpu?.usageSpeed || 'N/A'}%</td>
+                  <td className="float-right">{deviceDynamic?.cpu?.usageSpeed || 'N/A'}%</td>
                 </tr>
                 <tr className="border-bottom">
                   <td>Number of Processes</td>
-                  <td>{deviceDynamic?.cpu?.numProcesses || 'N/A'}</td>
+                  <td className="float-right">{deviceDynamic?.cpu?.numProcesses || 'N/A'}</td>
                 </tr>
                 <tr className="border-bottom">
                   <td>Threads Sleeping</td>
-                  <td>{deviceDynamic?.cpu?.threadsSleeping || 'N/A'}</td>
+                  <td className="float-right">{deviceDynamic?.cpu?.threadsSleeping || 'N/A'}</td>
                 </tr>
                 <tr className="border-bottom">
                   <td>Timestamp</td>
-                  <td>{
+                  <td className="float-right">{
                     deviceDynamic?.timestamp ?
                       format(new Date(deviceDynamic?.timestamp), 'MMM DD, YYYY, h:mm:ss A') :
                       'N/A'
