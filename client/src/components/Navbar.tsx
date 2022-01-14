cb791a0aca1ea564b1a98273918d642518ad06d2
Sacha Elkaim <37674225+sachaelkaim@users.noreply.github.com>
2022-01-14 13:24:41 -0500

    Client - changed navbar logic open/close

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {useRef} from 'react';
+import {useState} from 'react';
 // 3rd Party
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {Squash as Hamburger} from 'hamburger-react';
 import {DiRasberryPi} from 'react-icons/di';
@@ -9,9 +10,42 @@ import {CgProfile} from 'react-icons/cg';
 // Custom
 import {useAuth} from '../context/authContext';
 
+const SideNavData = [
+  {
+    title: 'Dashboard',
+    path: '/dashboard',
+    icon: <MdOutlineSpaceDashboard />,
+    cName: 'nav-item-wrapper',
+  },
+  {
+    title: 'Devices',
+    path: '/devices',
+    icon: <DiRasberryPi />,
+    cName: 'nav-item-wrapper',
+  },
+  {
+    title: 'Analytics',
+    path: '/analytics',
+    icon: <MdAnalytics />,
+    cName: 'nav-item-wrapper',
+  },
+  {
+    title: 'Profile',
+    path: '/profile',
+    icon: <CgProfile />,
+    cName: 'nav-item-wrapper',
+  },
+  {
+    title: 'Logout',
+    path: '/',
+    icon: <MdLogout />,
+    cName: 'nav-item-wrapper',
+  },
+];
+
 const Navbar = () => {
-  const sideMenu = useRef<any>();
-  const navBar = useRef<any>();
+  const [sideNav, setSideNav] = useState(false);
+  const showSideNav = () => setSideNav(!sideNav);
   const {setUser, setIsAuthenticated} = useAuth();
   const logout = async (e: React.ChangeEvent<any>) => {
     e.preventDefault();
@@ -28,55 +62,35 @@ const Navbar = () => {
         });
   };
 
-  const openNav = () => {
-    sideMenu.current.style.width = '250px';
-    navBar.current.style.position = 'fixed';
-    navBar.current.style.top = '0';
-  };
-
-  const closeNav = () => {
-    sideMenu.current.style.width = '0';
-    navBar.current.style.position = 'static';
-  };
-
   return (
     <>
-      <div className='sidenav' ref={sideMenu}>
-        <div className='d-flex nav-items-wrapper'>
-          <a className="nav-items-logo" href="/dashboard"><MdOutlineSpaceDashboard /></a>
-          <a className="nav-items-text" href="/dashboard">Dashboard</a>
-        </div>
-        <div className='d-flex nav-items-wrapper'>
-          <a className="nav-items-logo" href="/devices"><DiRasberryPi /></a>
-          <a className="nav-items-text" href="/devices">Devices</a>
-        </div>
-        <div className='d-flex nav-items-wrapper'>
-          <a className="nav-items-logo"><MdAnalytics /></a>
-          <a className="nav-items-text">Analytics</a>
-        </div>
-        <div className='d-flex nav-items-wrapper'>
-          <a className="nav-items-logo"><CgProfile /></a>
-          <a className="nav-items-text">Profile</a>
-        </div>
-        <div className='d-flex nav-items-wrapper'>
-          <a className="nav-items-logo"><MdLogout /></a>
-          <a className="nav-items-text" onClick={(e) => logout(e)}>Exit</a>
-        </div>
-        <DiRasberryPi />
-      </div>
-      <div
+      <nav
         className='d-flex nav-wrapper'
       >
-        <div className='nav-hamburger' ref={navBar}>
-          <Hamburger rounded color={'white'} size={33} onToggle={(toggled) => {
-            if (toggled) {
-              openNav();
-            } else {
-              closeNav();
-            }
+        <div className={sideNav ? 'nav-hamburger active' : 'nav-hamburger'}>
+          <Hamburger rounded color={'white'} size={33} onToggle={() => {
+            showSideNav();
           }} />
         </div>
-      </div>
+        <div className={sideNav ? 'side-nav active' : 'side-nav'}>
+          {SideNavData.map((item, index) => {
+            return (
+              <div key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.title == 'Logout' ?
+                    <span className="nav-item-logo" onClick={(e) => logout(e)}>{item.icon}</span> :
+                    <span className="nav-item-logo">{item.icon}</span>
+                  }
+                  {item.title == 'Logout' ?
+                    <span className="nav-item-text" onClick={(e) => logout(e)}>{item.title} </span> :
+                    <span className="nav-item-text">{item.title}</span>
+                  }
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </nav>
     </>
   );
 };
