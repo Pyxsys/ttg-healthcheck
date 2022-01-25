d0329ee8ff95caa15b9dcd29e001f5539da2a236
Derek <ruizc.derek@gmail.com>
2022-01-24 19:22:49 -0500

    Client - Add Modal Service To Global Services
    Modal Service used to display a modal with common features anywhere on the page

---
+++ client/src/context/modal.context.tsx
@@ -0,0 +1,134 @@
+import React, {useState} from 'react';
+import {Modal} from 'react-bootstrap';
+import Contextualizer, {AppServices} from '../services/context.service';
+
+interface ModalOptions {
+  width?: number
+  modalClassName?: string
+
+  showPrimaryButton?: boolean
+  primaryButtonText?: string
+  showSecondaryButton?: boolean
+  secondaryButtonText?: string
+}
+
+/**
+ * Calls the function, when a button is clicked.
+ */
+ type ButtonClickedCallback = () => void
+
+export interface IModalService {
+  open(component: JSX.Element, size?: 'sm' | 'lg' | 'xl' | 'full', options?: ModalOptions): void
+  close(): void
+  onPrimaryClicked: ButtonClickedCallback
+  onSecondaryClicked: ButtonClickedCallback
+}
+
+
+const ModalContext = Contextualizer.createContext<IModalService>(
+    AppServices.ModalService,
+);
+export const useModalService = () =>
+  Contextualizer.use<IModalService>(AppServices.ModalService);
+
+const ModalService = ({children}: any) => {
+  const [primaryText, setPrimaryText] = useState('Save');
+  const [secondaryText, setSecondaryText] = useState('Cancel');
+
+  const [show, setShow] = useState(false);
+  const [modalContent, setModalContent] = useState(<></>);
+  const [size, setSize] = useState('lg' as 'sm' | 'lg' | 'xl' | undefined);
+  const [fullscreen, setFullscreen] = useState(false);
+
+  const [stylingClass, setStylingClass] = useState('');
+  // const [bodyClassName, setBodyClassName] = useState('');
+
+  // const options = {};
+  const modalService: IModalService = {
+    open: (component, size = 'lg', options) => {
+      // Modal Size
+      if (size === 'full') {
+        setFullscreen(true);
+        setSize(undefined);
+      } else {
+        setFullscreen(false);
+        setSize(size);
+      }
+
+      // Styling
+      let styling = '';
+      if (options?.width) {
+        styling += `modal-custom-width w-${options.width}`;
+        setSize(undefined);
+        setFullscreen(false);
+      }
+      if (options?.modalClassName) {
+        styling += options.modalClassName;
+        setSize(undefined);
+        setFullscreen(false);
+      }
+
+      // Footer Buttons
+      if (options?.showPrimaryButton === false) {
+        setPrimaryText('');
+      } else if (options?.primaryButtonText) {
+        setPrimaryText(options.primaryButtonText);
+      }
+      if (options?.showSecondaryButton === false) {
+        setSecondaryText('');
+      } else if (options?.secondaryButtonText) {
+        setSecondaryText(options.secondaryButtonText);
+      }
+
+      setStylingClass(styling);
+      setModalContent(component);
+      setShow(true);
+    },
+
+    close: () => {
+      setShow(false);
+    },
+
+    onPrimaryClicked: () => {},
+    onSecondaryClicked: () => {},
+  };
+
+  return (
+    <>
+      <ModalContext.Provider value={modalService}>
+        {children}
+      </ModalContext.Provider>
+
+      <Modal
+        show={show}
+        size={size}
+        fullscreen={fullscreen ? true : undefined}
+        onHide={() => setShow(false)}
+        centered
+        dialogClassName={stylingClass}
+      >
+        <Modal.Body className='modal-background text-white'>{modalContent}</Modal.Body>
+        {primaryText || secondaryText ?
+          <Modal.Footer className='modal-background d-flex justify-flex-end'>
+            {secondaryText ?
+              <button className='btn btn-secondary' onClick={() => {
+                modalService?.onSecondaryClicked();
+                setShow(false);
+              }}>{secondaryText}</button> :
+              <></>
+            }
+            {primaryText ?
+              <button className='btn btn-primary ms-5' onClick={() => {
+                modalService?.onPrimaryClicked();
+                setShow(false);
+              }}>{primaryText}</button> :
+              <></>
+            }
+          </Modal.Footer> :
+          <></>
+        }
+      </Modal>
+    </>
+  );
+};
+export default ModalService;
