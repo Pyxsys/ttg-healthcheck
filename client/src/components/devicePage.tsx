1d5e64c67c310fb94416e596c08590a2d262a6aa
Derek <ruizc.derek@gmail.com>
2022-01-26 20:54:06 -0500

    Devices Table - Update Devices Table To not Use Bootsrap
    Can now scroll in the table, and the table fits better in the page

--- client/src/components/devicePage.tsx
+++ client/src/components/devicePage.tsx
@@ -1,10 +1,7 @@
 // 3rd Party
-import React, {useEffect, useState} from 'react';
+import React, {Dispatch, SetStateAction, useEffect, useState} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
-import {Col, Row} from 'react-bootstrap';
-import BootstrapTable from 'react-bootstrap-table-next';
-import filterFactory, {textFilter} from 'react-bootstrap-table2-filter';
 import {BsChevronLeft, BsChevronRight} from 'react-icons/bs';
 
 // Custom
@@ -16,11 +13,25 @@ import {SignalStrength, signalText} from './common/signalStrength';
 import {TableDevice} from '../types/tables';
 
 
+const useDebounce = (initialValue = '', delay: number) => {
+  const [actualValue, setActualValue] = useState(initialValue);
+  const [debounceValue, setDebounceValue] = useState(initialValue);
+  useEffect(() => {
+    const debounceId = setTimeout(() => setDebounceValue(actualValue), delay);
+    return () => clearTimeout(debounceId);
+  }, [actualValue, delay]);
+  return [actualValue, debounceValue, setActualValue] as [string, string, Dispatch<SetStateAction<string>>];
+};
+
 const DevicePage = () => {
   // Readonly Values
   const initialPage: number = 1;
   const pageSize: number = 10;
+  const initialOrderBy: string = 'Name';
 
+  const [orderBy, setOrderBy] = useState(initialOrderBy);
+  const [orderByAsc, setOrderByAsc] = useState(true);
+  const [actualFilter, delayedFilter, setFilter] = useDebounce('', 500);
 
   const [deviceTableData, setDeviceTableData] = useState([] as TableDevice[]);
   const [page, setPage] = useState(initialPage);
@@ -40,14 +51,7 @@ const DevicePage = () => {
   };
 
   const queryTable = async () => {
-    const skip: number = (page - 1) * pageSize;
-    const deviceQuery = {
-      params: {
-        limit: pageSize,
-        skip: skip,
-        Total: true,
-      },
-    };
+    const deviceQuery = {params: {Total: true}};
     const deviceResponse = await axios.get<IResponse<Device>>(
         'api/device',
         deviceQuery,
@@ -75,11 +79,8 @@ const DevicePage = () => {
 
   useEffect(() => {
     initialRealTimeData();
-  }, []);
-
-  useEffect(() => {
     queryTable();
-  }, [page]);
+  }, []);
 
   /**
    * If the browser window unloads
@@ -95,136 +96,41 @@ const DevicePage = () => {
     };
   }, []);
 
-  type cell = any | null | undefined
-
-  const idFormatter = (cell: cell) => {
-    return (
-      <div className="devices-column-h d-flex justify-content-left align-items-center">
-        <div className="devices-uuid-text devices-font">
-          <Link
-            className="text-white"
-            to={{pathname: '/device', state: {id: cell}}}
-          >
-            {cell}
-          </Link>
-        </div>
-      </div>
-    );
+  const filterTable = (device: TableDevice): boolean => {
+    return device.static.deviceId.toLocaleLowerCase().includes(delayedFilter.toLocaleLowerCase());
   };
 
-  const nameFormatter = (cell: cell) => {
-    return (
-      <div className="devices-column-h d-flex justify-content-left align-items-center">
-        <div className="devices-uuid-text devices-font">
-          {cell}
-        </div>
-      </div>
-    );
+  const sortTable = (device1: TableDevice, device2: TableDevice): number => {
+    const device1Swapped = orderByAsc ? device2 : device1;
+    const device2Swapped = orderByAsc ? device1 : device2;
+    switch (orderBy) {
+      case 'UUID':
+        return device1Swapped.static.deviceId.localeCompare(device2Swapped.static.deviceId);
+      case 'Name':
+        return (device1Swapped.static.name || '').localeCompare(device2Swapped.static.name || '');
+      case 'CPU':
+        return (device1Swapped.dynamic?.cpu?.aggregatedPercentage || -1) - (device2Swapped.dynamic?.cpu?.aggregatedPercentage || -1);
+      case 'Memory':
+        return (device1Swapped.dynamic?.memory?.aggregatedPercentage || -1) - (device2Swapped.dynamic?.memory?.aggregatedPercentage || -1);
+      case 'Disk':
+        return (device1Swapped.dynamic?.disk?.partitions[0]?.percent || -1) - (device2Swapped.dynamic?.disk?.partitions[0]?.percent || -1);
+      case 'Network':
+        const signal1 = device1Swapped.dynamic?.wifi?.signalStrength === undefined ? -1 : Number(device1Swapped.dynamic?.wifi?.signalStrength);
+        const signal2 = device2Swapped.dynamic?.wifi?.signalStrength === undefined ? -1 : Number(device2Swapped.dynamic?.wifi?.signalStrength);
+        return signal1 - signal2;
+      default:
+        return 0;
+    }
   };
 
-  const pieUsageFormatter = (cell: cell) => {
-    return (
-      <div className="d-flex justify-content-end align-items-center">
-        <div className="text-truncate devices-font">
-          {Number(cell).toFixed(2)}
-          {cell ? '%' : ''}
-        </div>
-        <div className="ps-2 devices-column">
-          <PieWheel percentage={Number(cell)} text={false} />
-        </div>
-      </div>
-    );
+  const selectOrderBy = (order: string): void => {
+    if (orderBy === order) {
+      setOrderByAsc(!orderByAsc);
+    } else {
+      setOrderBy(order);
+      setOrderByAsc(true);
+    }
   };
-
-  const signalStrengthFormatter = (cell: cell) => {
-    return (
-      <div className="d-flex flex-column">
-        <div className="d-flex justify-content-end align-items-center">
-          <div className="text-truncate devices-font">
-            {signalText(Number(cell))}
-          </div>
-          <div className="ps-2 devices-column">
-            <SignalStrength level={Number(cell)} showText={false} />
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const uuidFirstHeaderFormatter = (
-      column: any,
-      colIndex: any,
-      {sortElement, filterElement}: any,
-  ) => {
-    return (
-      <div className="devices-first-header-formatter">
-        {column.text}
-        {sortElement}
-        <span className='ps-2'>{filterElement}</span>
-      </div>
-    );
-  };
-
-  const uuidHeaderFormatter = (
-      column: any,
-      colIndex: any,
-      {sortElement, filterElement}: any,
-  ) => {
-    return (
-      <div className="devices-table-header-formatter">
-        {column.text}
-        {sortElement}
-      </div>
-    );
-  };
-
-  const columns = [
-    {
-      dataField: 'static.deviceId',
-      text: 'UUID',
-      filter: textFilter({
-        placeholder: 'Filter by UUID...',
-      }),
-      sort: true,
-      formatter: idFormatter,
-      headerFormatter: uuidFirstHeaderFormatter,
-    },
-    {
-      dataField: 'static.name',
-      text: 'Name',
-      sort: true,
-      formatter: nameFormatter,
-      headerFormatter: uuidHeaderFormatter,
-    },
-    {
-      dataField: 'dynamic.cpu.aggregatedPercentage',
-      text: 'CPU',
-      sort: true,
-      formatter: pieUsageFormatter,
-      headerFormatter: uuidHeaderFormatter,
-    },
-    {
-      dataField: 'dynamic.memory.aggregatedPercentage',
-      text: 'Memory',
-      sort: true,
-      formatter: pieUsageFormatter,
-      headerFormatter: uuidHeaderFormatter,
-    },
-    {
-      dataField: 'dynamic.disk.aggregatedPercentage',
-      text: 'Disk',
-      sort: true,
-      formatter: pieUsageFormatter,
-      headerFormatter: uuidHeaderFormatter,
-    },
-    {
-      dataField: 'dynamic.wifi.signalStrength',
-      text: 'Network',
-      sort: true,
-      formatter: signalStrengthFormatter,
-      headerFormatter: uuidHeaderFormatter,
-    },
-  ];
 
   return (
     <div className="h-100 d-flex flex-column">
@@ -232,41 +138,131 @@ const DevicePage = () => {
         <Navbar />
       </div>
 
-      <div className="flex-grow-1 d-flex flex-column align-items-center devices-content">
-        <div id="page-wrap" className="h-100 overflow-auto container">
-          <Row className="flex-nowrap h-100">
-            <Col>
-              <div className="devices-table ">
-                <BootstrapTable
-                  striped={true}
-                  keyField="static.deviceId"
-                  data={deviceTableData}
-                  columns={columns}
-                  filter={filterFactory()}
-                  sort={{dataField: 'static.deviceId', order: 'desc'}}
-                />
-                <div className="d-flex justify-content-end">
-                  <i
-                    className="pe-2 device-icon"
-                    role="button"
-                    onClick={() => setPage(page > 1 ? page - 1 : 1)}
-                  >
-                    <BsChevronLeft />
-                  </i>
-                  <span className="device-span">Page {page}</span>
-                  <i
-                    className="ps-2 device-icon"
-                    role="button"
-                    onClick={() => setPage(page < totalPages ? page + 1 : page)}
-                  >
-                    <BsChevronRight />
-                  </i>
-                </div>
-              </div>
-            </Col>
-          </Row>
+      <div className="flex-grow-1 d-flex flex-column align-items-center overflow-auto devices-content">
+        <div className='flex-grow-1 d-flex flex-column overflow-auto container'>
+          <div className='flex-grow-1 overflow-auto table-container mt-5 p-1'>
+            {delayedFilter}
+            <table className="cerebellum-table table-striped text-white overflow-auto w-100 m-1">
+              <thead>
+                <tr className='sticky-header'>
+                  {['UUID', 'Name', 'CPU', 'Memory', 'Disk', 'Network'].map((column) =>
+                    <th key={column} tabIndex={0} className='cursor-pointer' onClick={() => selectOrderBy(column)}>
+                      <div className='d-flex align-items-center'>
+                        <span>{column}</span>
+                        <div className='ps-2'>
+                          {orderBy === column ?
+                            <>{orderByAsc ?
+                                <span className='dropdown px-1'><i className='caret'></i></span> :
+                                <span className='dropup px-1'><i className='caret'></i></span>}
+                            </> :
+                            <>
+                              <div className="d-flex">
+                                <span className='dropdown'><i className='caret'></i></span>
+                                <span className='dropup'><i className='caret'></i></span>
+                              </div>
+                            </>
+                          }
+                        </div>
+                        {column === 'UUID' ?
+                          <label className='ps-2 user-select-none'>
+                            <input type="text" className="form-control" placeholder="Filter by UUID..." value={actualFilter}
+                              onClick={(e) => e.stopPropagation()} onChange={(e) => setFilter(e.target.value)} />
+                          </label> :
+                          <></>
+                        }
+                      </div>
+                    </th>,
+                  )}
+                </tr>
+              </thead>
+              <tbody className='overflow-auto'>
+                {deviceTableData
+                    .filter(filterTable)
+                    .sort(sortTable)
+                    .slice((page-1)*pageSize, (page-1)*pageSize+pageSize)
+                    .map((device) =>
+                      <tr key={device.static.deviceId}>
+                        <td>
+                          <div className="devices-uuid-text devices-font mx-auto h-100 py-3">
+                            <Link className="text-white"
+                              to={{pathname: '/device', state: {id: device.static.deviceId}}}
+                            >
+                              {device.static.deviceId}
+                            </Link>
+                          </div>
+                        </td>
+                        <td>
+                          <span className="devices-font">
+                            {device.static.name}
+                          </span>
+                        </td>
+                        <td>
+                          <div className="d-flex justify-content-end align-items-center">
+                            <div className="text-truncate devices-font">
+                              {Number(device.dynamic?.cpu.aggregatedPercentage).toFixed(2)}
+                              {device.dynamic?.cpu.aggregatedPercentage ? '%' : ''}
+                            </div>
+                            <div className="ps-2 pie-wheel-size">
+                              <PieWheel percentage={Number(device.dynamic?.cpu.aggregatedPercentage)} text={false} />
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <div className="d-flex justify-content-end align-items-center">
+                            <div className="text-truncate devices-font">
+                              {Number(device.dynamic?.memory.aggregatedPercentage).toFixed(2)}
+                              {device.dynamic?.memory.aggregatedPercentage ? '%' : ''}
+                            </div>
+                            <div className="ps-2 pie-wheel-size">
+                              <PieWheel percentage={Number(device.dynamic?.memory.aggregatedPercentage)} text={false} />
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <div className="d-flex justify-content-end align-items-center">
+                            <div className="text-truncate devices-font">
+                              {Number(device.dynamic?.disk?.partitions[0].percent).toFixed(2)}
+                              {device.dynamic?.disk?.partitions[0].percent ? '%' : ''}
+                            </div>
+                            <div className="ps-2 pie-wheel-size">
+                              <PieWheel percentage={Number(device.dynamic?.disk?.partitions[0].percent)} text={false} />
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <div className="d-flex flex-column">
+                            <div className="d-flex justify-content-end align-items-center">
+                              <div className="text-truncate devices-font">
+                                {signalText(Number(device.dynamic?.wifi.signalStrength))}
+                              </div>
+                              <div className="ps-2 pie-wheel-size">
+                                <SignalStrength level={Number(device.dynamic?.wifi.signalStrength)} showText={false} />
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                      </tr>,
+                    )
+                }
+              </tbody>
+            </table>
+          </div>
+
+          <div className="d-flex pt-2 ms-auto">
+            <i className={`pe-2 device-icon ${page > 1 ? 'cursor-pointer' : ''}`}
+              onClick={() => setPage(page > 1 ? page - 1 : 1)}
+            >
+              <BsChevronLeft />
+            </i>
+            <span className="device-span user-select-none">Page {page} of {totalPages}</span>
+            <i className={`ps-2 device-icon ${page < totalPages ? 'cursor-pointer' : ''}`}
+              onClick={() => setPage(page < totalPages ? page + 1 : page)}>
+              <BsChevronRight />
+            </i>
+          </div>
         </div>
       </div>
+
       <div className="d-flex justify-content-center devices-footer">
         <div className="pt-1 pb-3 devices-copyright">
           &#169; SOEN490 TTG-HEALTCHECK
