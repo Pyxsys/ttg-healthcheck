392491025da88b1adcf4e4897fd19f69891d25c4
shashank <shashank.patel@aheeva.com>
2022-01-29 18:41:23 -0500

    code smell fix - Remove this unused import

--- cypress/integration/performance/login.js
+++ cypress/integration/performance/login.js
@@ -1,7 +1,5 @@
 /* eslint-disable jest/expect-expect */
 Cypress.env();
-import runLogin from './common/runLogin'
-import checkPageLoadTime from './common/checkPageLoadTime'
 import {lighthouseThreshold, lighthouseConfig} from './common/lighthouseThresholds'
 
 
