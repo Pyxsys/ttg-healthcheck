25d892bce2da58248613438750a17ee2d819d46d
Derek <ruizc.derek@gmail.com>
2022-03-16 20:36:52 -0400

    FE Common - Table Can Disable Order By On A Column

--- client/src/components/common/viewTable.tsx
+++ client/src/components/common/viewTable.tsx
@@ -2,15 +2,8 @@
 import React, {Dispatch, SetStateAction, useEffect, useState} from 'react';
 
 // Custom
-import {IColumnDetail} from '../../types/tables';
+import {ViewTableInputs} from '../../types/tables';
 
-interface ViewTableInputs<T> {
-  tableData: T[]
-  columns: IColumnDetail[]
-  page?: number
-  pageSize?: number
-  initialOrderBy?: string
-}
 
 const useDebounce = (initialValue: string, delay: number) => {
   const [actualValue, setActualValue] = useState(initialValue);
@@ -115,12 +108,16 @@ const ViewTable = (props: ViewTableInputs<any>) => {
             <th
               key={column.key}
               tabIndex={0}
-              className="cursor-pointer"
-              onClick={() => selectOrderBy(column.key)}
+              className={column.disableOrderBy ? '' : 'cursor-pointer'}
+              onClick={() => column.disableOrderBy ? null : selectOrderBy(column.key)}
             >
               <div className="d-flex align-items-center">
                 <span>{column.name}</span>
-                <div className="ps-2">{orderByIcon(column.key)}</div>
+                {column.disableOrderBy ? (
+                  <></>
+                ) : (
+                  <div className="ps-2">{orderByIcon(column.key)}</div>
+                )}
                 {column.filter ? (
                   <label className="ps-2 user-select-none">
                     <input
