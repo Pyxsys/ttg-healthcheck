921d7aaa0d7f387652b4d4c467ec2e6c75b6828e
rex884 <mikeyrex884@gmail.com>
2022-04-01 14:26:15 -0400

    Updated Profile Page user tests
    2 tests added to front end of profile page for user and admin

--- cypress/integration/front-end/userProfile.page.js
+++ cypress/integration/front-end/userProfile.page.js
@@ -5,9 +5,9 @@ const admintestUser = {
   email: "admin@gmail.com"
 };
 const user = {
-  name: "Michael Takenaka",
-  password: Cypress.env("TailsIKnowWhoYour"),
-  email: "mikeyrex884@gmail.com"
+  name: "A Name",
+  password: "test",
+  email: "a@gmail.com"
 };
 
 describe("1. if you're an admin you will see a table with all users when you click on profile", () => {
@@ -18,51 +18,62 @@ describe("1. if you're an admin you will see a table with all users when you cli
     cy.get("input[name=email1]").type(admintestUser.email);
     cy.get("input[name=password1]").type(admintestUser.password);
     cy.get("button[type=submit]").click();
-    cy.url().should("include", "dashboard");
 
     cy.get('div[class="dropdown"]').click();
     cy.get('a[href*="/admin"]').click();
 
-    cy.url().should("include", "");
+    cy.get(".devices-content > .flex-column").should("be.visible"); //visible
   });
 
   it(" 1.1. you can click on any use and edit their profile", () => {
-    cy.get("input[name=email1]").type(admintestUser.email);
-    cy.get("input[name=password1]").type(admintestUser.password);
-    cy.get("button[type=submit]").click();
-    cy.url().should("include", "dashboard");
+    cy.get(".devices-content > .flex-column")
+      .contains("logtestuser1234")
+      .click();
+    cy.get(
+      ":nth-child(1) > :nth-child(1) > tbody > tr > :nth-child(2) > .btn"
+    ).click();
+    cy.get(
+      "#account-info-form > table > tbody > :nth-child(1) > td > .input-group > .user-profile-input"
+    )
+      .clear()
+      .type("ChangedName");
+    cy.get(":nth-child(3) > .btn").click();
 
     cy.get('div[class="dropdown"]').click();
     cy.get('a[href*="/admin"]').click();
+    cy.get(".devices-content > .flex-column").contains("ChangedName");
+  });
 
-    cy.url().should("include", "");
+  after(() => {
+    cy.get(".devices-content > .flex-column")
+      .contains("ChangedName")
+      .click();
+    cy.get(
+      ":nth-child(1) > :nth-child(1) > tbody > tr > :nth-child(2) > .btn"
+    ).click();
+    cy.get(
+      "#account-info-form > table > tbody > :nth-child(1) > td > .input-group > .user-profile-input"
+    )
+      .clear()
+      .type("logtestuser1234");
+    cy.get(":nth-child(3) > .btn").click();
   });
 });
 
 describe("2. if you're an user you will see your profile when you click on view profile button", () => {
-  it("Access profile as regular user", () => {
+  it("User profile page", () => {
+    const loginPage = "/";
+    cy.visit(loginPage);
+
     cy.get("input[name=email1]").type(user.email);
     cy.get("input[name=password1]").type(user.password);
     cy.get("button[type=submit]").click();
-    cy.url().should("include", "dashboard");
 
     cy.get('div[class="dropdown"]').click();
-    cy.get('a[href*="/admin"]').click();
+    cy.get(
+      '[href="/user-profile?Id=623ac85285aadcd23d52f26e"] > .nav-right-font'
+    ).click();
+
+    cy.get(".user-profile-content > .flex-column").should("be.visible");
   });
 });
-
-/*
-describe("AdminPanel Page", () => {
-  it("After successful login user with admin privileges can access /adminpanel page", () => {
-    cy.visit("/");
-
-    cy.get("input[name=email1]").type(admintestUser.email);
-    cy.get("input[name=password1]").type(admintestUser.password);
-    cy.get("button[type=submit]").click();
-    cy.url().should("include", "dashboard");
-
-    cy.visit("/adminpanel");
-    cy.url().should("include", "adminpanel");
-  });
-});
-*/
