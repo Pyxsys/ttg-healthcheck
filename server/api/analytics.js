c0543b1e06279e086ad8be62ace299851a081e52
Ashraf Khalil <60198939+ashrafwkhalil@users.noreply.github.com>
2022-04-02 13:02:49 -0400

    removed unnecessary endpoint

--- server/api/analytics.js
+++ server/api/analytics.js
@@ -24,45 +24,6 @@ router.get('/devicesByMem', auth, async(req, res) => {
 
   
 })
-router.get('/graphWeek', auth, async(req,res) => {
-  const query = Object(req.query)
-  const addDaysToToday = (i) => {
-    let date1 = new Date();
-    let date2 = new Date();
-    date2.setDate(date1.getDate() + i)
-    return date2;
-  }
-  // If query does not have Ids attribute
-  if (!query.Ids) {
-    if (query.Ids === undefined) {
-      return res.status(501).send('Server Error: must include Ids parameter')
-    } else {
-      return res.status(200).json({ Results: [] })
-    }
-  }
-
-  const idsArray = String(query.Ids).split(',')
-  const averages = [[]];
-  counter = 0;
-  const results = await Promise.all(
-    idsArray.forEach(async (id) => {
-      counter++;
-      for (let i = 0; i < 7; i++){
-      const deviceDayAverage = await DeviceLogs.aggregate([
-        { $match : { deviceId: id , timestamp: {$lt: addDaysToToday(1-i), $gte: addDaysToToday(-1-i)}}},
-        {$group: {_id: "$deviceId", 
-      average: {$avg: query.metric}}},
-      ])
-      averages[counter][i] = deviceDayAverage;
-      }
-    })
-  )
-  const nonEmptyResults = averages.filter((e) => e)
-
-  return res.status(200).json({ Results: nonEmptyResults })
-    
-  
-})
 // get latest device logs from a list of device Ids
 router.get('/latest', auth, async (req, res) => {
   const query = Object(req.query)
