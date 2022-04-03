aeef2ec5a7996d15a3d4885b519a12a47f3864d6
NathanZiri <37674081+NathanZiri@users.noreply.github.com>
2022-04-02 20:37:09 -0400

    Removed Duplicate Code

--- cypress/integration/end-to-end/performance/analytics.js
+++ cypress/integration/end-to-end/performance/analytics.js
@@ -14,6 +14,24 @@ const testUser = {
   email: "cypress@gmail.com"
 };
 
+const performanceWidget = index => {
+  cy.get(".gx-4 > :nth-child(" + index + ")")
+    .find("input")
+    .should("be.visible");
+  cy.get(".gx-4 > :nth-child(" + index + ")")
+    .find("table")
+    .should("be.visible")
+    .contains("223ECC81-7069-45F9-9309-C641EBA776C8")
+    .should("be.visible");
+};
+
+const performanceGraph = index => {
+  cy.get(".analytics-accordion.analytics-accordion-padding")
+    .find(".graph-accordion-body > :nth-child(1) > :nth-child(1)")
+    .find(".settings-body-wrapper > :nth-child(" + index + ")")
+    .should("be.visible");
+};
+
 describe("1. LightHouse", () => {
   it("1.1 dashboard Page", () => {
     cy.visit("/");
@@ -29,58 +47,24 @@ describe("2. Check to ensure that the various pages related to login load in a r
       .its("performance")
       .then(performance => {
         cy.viewport(1920, 1200);
-        cy.get(".gx-4 > :nth-child(1)")
-          .find("input")
-          .should("be.visible");
-        cy.get(".gx-4 > :nth-child(1)")
-          .find("table")
-          .should("be.visible")
-          .contains("223ECC81-7069-45F9-9309-C641EBA776C8")
-          .should("be.visible");
+        performanceWidget(1);
         cy.get(".gx-4 > :nth-child(1)")
           .contains("Overworked CPU Devices")
           .should("be.visible");
-
-        cy.get(".gx-4 > :nth-child(2)")
-          .find("input")
-          .should("be.visible");
-        cy.get(".gx-4 > :nth-child(2)")
-          .find("table")
-          .should("be.visible")
-          .contains("223ECC81-7069-45F9-9309-C641EBA776C8")
-          .should("be.visible");
+        performanceWidget(2);
         cy.get(".gx-4 > :nth-child(2)")
           .contains("Overworked Memory Devices")
           .should("be.visible");
-
-        cy.get(".gx-4 > :nth-child(3)")
-          .find("input")
-          .should("be.visible");
-        cy.get(".gx-4 > :nth-child(3)")
-          .find("table")
-          .should("be.visible")
-          .contains("223ECC81-7069-45F9-9309-C641EBA776C8")
-          .should("be.visible");
+        performanceWidget(3);
         cy.get(".gx-4 > :nth-child(3)")
           .contains("Network Speed Monitoring")
           .should("be.visible");
 
         cy.get(".analytics-details-wrapper").scrollTo("bottom");
 
-        cy.get(".analytics-accordion.analytics-accordion-padding")
-          .find(".graph-accordion-body > :nth-child(1) > :nth-child(1)")
-          .find(".settings-body-wrapper > :nth-child(2)")
-          .should("be.visible");
-
-        cy.get(".analytics-accordion.analytics-accordion-padding")
-          .find(".graph-accordion-body > :nth-child(1) > :nth-child(1)")
-          .find(".settings-body-wrapper > :nth-child(4)")
-          .should("be.visible");
-
-        cy.get(".analytics-accordion.analytics-accordion-padding")
-          .find(".graph-accordion-body > :nth-child(1) > :nth-child(1)")
-          .find(".settings-body-wrapper > :nth-child(6)")
-          .should("be.visible");
+        performanceGraph(2);
+        performanceGraph(4);
+        performanceGraph(6);
 
         cy.get(".analytics-accordion.analytics-accordion-padding")
           .find(".graph-accordion-body > :nth-child(1) > :nth-child(2)")
@@ -91,7 +75,6 @@ describe("2. Check to ensure that the various pages related to login load in a r
             ".graph-accordion-body > :nth-child(2) > .analytics-accordion-padding"
           )
           .should("be.visible")
-
           .then(() => performance.mark("end-loading"))
           .then(() => {
             checkPageLoadTime(performance, 3000);
