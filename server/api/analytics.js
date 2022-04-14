18547fbc99b6963ddcf7497aff869f6ae65ec9c5
Derek <ruizc.derek@gmail.com>
2022-04-13 20:51:06 -0400

    Server - Analytics Remove Code Duplication

--- server/api/analytics.js
+++ server/api/analytics.js
@@ -15,34 +15,6 @@ router.get('/', auth, async (req, res) => {
   return res.status(200).json(deviceLogsResponse)
 })
 
-// get latest device logs from a list of device Ids
-router.get('/latest', auth, async (req, res) => {
-  const query = Object(req.query)
-
-  // If query does not have Ids attribute
-  if (!query.Ids) {
-    if (query.Ids === undefined) {
-      return res.status(501).send('Server Error: must include Ids parameter')
-    } else {
-      return res.status(200).json({ Results: [] })
-    }
-  }
-
-  const idsArray = String(query.Ids).split(',')
-  const results = await Promise.all(
-    idsArray.map(async (id) => {
-      const devices = await DeviceLogs.find(
-        { deviceId: id },
-        {},
-        { limit: 1, sort: { timestamp: [-1] } }
-      )
-      return devices.length > 0 ? devices[0] : null
-    })
-  )
-  const nonEmptyResults = results.filter((deviceLog) => deviceLog)
-
-  return res.status(200).json({ Results: nonEmptyResults })
-})
 router.get('/afterDate', auth, async (req, res) => {
   const query = Object(req.query)
   const addDaysToToday = (i) => {
